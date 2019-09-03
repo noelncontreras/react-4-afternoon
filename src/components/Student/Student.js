ec9cb501fa11bf4423ac83d901b36688a05c276a
Noel N. Contreras <noeln.contreras@Noels-MacBook-Air.local>
2019-09-03 16:13:37 -0500

    finished project

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 export default class Student extends Component {
-  constructor() {
-    super()
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      studentInfo: {}
+    }
   }
+
+  componentDidMount() {
+    return axios
+      .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(results => {
+        this.setState({studentInfo: results.data})
+      })
+  }
+
+  // handleClick = () => {
+  //   this.props.history.goBack()
+  // }
 
   render() {
     return (
       <div className="box">
-        <h1>Student</h1>
+        <h1>Student:</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
+          <button onClick={() => {
+            this.props.history.goBack()}}>Back</button>
       </div>
     )
   }
